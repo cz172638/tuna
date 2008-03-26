0870e1f2dd53b24a38a8568da202997648db28f9
Arnaldo Carvalho de Melo <acme@redhat.com>
2008-03-26 18:43:41 -0300

    [TUNA]: Support command line options
    So that tuna can be used without pygtk2 and be useful in scripting.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tuna-cmd.py
+++ tuna-cmd.py
@@ -13,14 +13,53 @@
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #   General Public License for more details.
 
-import tuna
+import getopt, procfs, sys, tuna
 
-def main():
+nr_cpus = None
+
+def usage():
+	print '''Usage: tuna [OPTIONS]
+	-h, --help             Give this help list
+	-g, --gui              Start the GUI
+	-i, --isolate_cpu=CPU  Move all threads away from CPU'''
+
+def gui():
 	try:
 		app = tuna.tuna()
 		app.run()
 	except KeyboardInterrupt:
 		pass
+
+def get_nr_cpus():
+	global nr_cpus
+	if nr_cpus:
+		return nr_cpus
+	nr_cpus = procfs.cpuinfo().nr_cpus
+	return nr_cpus
+
+def main():
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],
+					   "i:gh",
+					   ( "isolate_cpus=", "gui", "help", ))
+	except getopt.GetoptError, err:
+		usage()
+		print str(err)
+		sys.exit(2)
+
+	if not opts:
+		gui()
+		return
+
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			usage()
+			return
+		elif o in ("-g", "--gui"):
+			gui()
+			return
+		elif o in ("-i", "--isolate_cpus"):
+			tuna.isolate_cpu(int(a), get_nr_cpus())
 
 if __name__ == '__main__':
     main()
